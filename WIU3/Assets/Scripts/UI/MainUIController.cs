d6979a8c7923d0cc5a474871ce3629cd4f46e238
SirPoopsalot1 <ngimmanuel979@gmail.com>
2024-08-12 12:52:39 +0800

    update good luck comrade

--- WIU3/Assets/Scripts/UI/MainUIController.cs
+++ WIU3/Assets/Scripts/UI/MainUIController.cs
@@ -2,28 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Arcade.UI
+/// <summary>
+/// A simple controller for switching between UI panels.
+/// </summary>
+public class MainUIController : MonoBehaviour
 {
-    /// <summary>
-    /// A simple controller for switching between UI panels.
-    /// </summary>
-    public class MainUIController : MonoBehaviour
+    public GameObject[] panels;
+
+    public void SetActivePanel(int index)
     {
-        public GameObject[] panels;
-
-        public void SetActivePanel(int index)
+        for (var i = 0; i < panels.Length; i++)
         {
-            for (var i = 0; i < panels.Length; i++)
-            {
-                var active = i == index;
-                var g = panels[i];
-                if (g.activeSelf != active) g.SetActive(active);
-            }
+            var active = i == index;
+            var g = panels[i];
+            if (g.activeSelf != active)
+                g.SetActive(active);
         }
+    }
 
-        void OnEnable()
-        {
-            SetActivePanel(0);
-        }
+    void OnEnable()
+    {
+        SetActivePanel(0);
     }
 }
