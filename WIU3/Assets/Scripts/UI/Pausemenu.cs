c0167a0ed556340073022106590263945581256a
SirPoopsalot1 <ngimmanuel979@gmail.com>
2024-08-19 09:53:49 +0800

    PauseUpdate

--- WIU3/Assets/Scripts/UI/Pausemenu.cs
+++ WIU3/Assets/Scripts/UI/Pausemenu.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Pausemenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-
+    public GameObject OptionmenuUI;
+    public GameObject ControlmenuUI;
+    public GameObject CreditmenuUI;
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
@@ -21,30 +23,44 @@ public class Pausemenu : MonoBehaviour
     }
     public void Resume()
     {
+        Debug.Log("Activated Resume");
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     private void Pause()
     {
+        Debug.Log("Activated Pause");
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
     public void Options()
     {
+        OptionmenuUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
         Debug.Log("Loading Options....");
     }
     public void Controls()
     {
+        ControlmenuUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
         Debug.Log("Loading Controls....");
     }
     public void Credits()
     {
+        CreditmenuUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
         Debug.Log("Loading Credits....");
     }
     public void QuitGame()
     {
+        pauseMenuUI.SetActive(false);
         Debug.Log("Quiting Game....");
+    }
+    public void back(GameObject CurrentGameMenuUI)
+    {
+        Pause();
+        CurrentGameMenuUI.SetActive(false);
     }
 }
