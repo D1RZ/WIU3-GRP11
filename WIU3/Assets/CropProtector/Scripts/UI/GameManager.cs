7836534287a6ad69e0880766d762c861ec37df26
D1RZ <Darkdevilfire123123@Gmail.com>
2024-08-20 15:41:26 +0800

    Addition of star animation

--- WIU3/Assets/CropProtector/Scripts/UI/GameManager.cs
+++ WIU3/Assets/CropProtector/Scripts/UI/GameManager.cs
@@ -38,21 +38,43 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] AudioClip GameOverSound;
 
-    [SerializeField] GameObject BGM;
+    [SerializeField] AudioClip WinGameSound;
+
+    [SerializeField] GameObject InGameBGM;
+
+    [SerializeField] GameObject VictoryBGM;
 
     [SerializeField] GameObject AudioSettingsPanel;
+
+    [SerializeField] CropSoundManager cropSoundManager;
+
+    [SerializeField] GameObject Star1;
+
+    [SerializeField] GameObject Star2;
+
+    [SerializeField] GameObject Star3;
 
     private AudioSettingsManager audioSettingsManager;
 
     public int LocustSpawnCount = 0;
 
-    public int LocustMaxCount = 10;
+    public int LocustMaxCount = 5;
 
-    public int LocustCount = 10;
+    public int LocustCount = 5;
 
     private float CurrentWave = 1;
 
     bool GameOver = false;
+
+    bool Star1AnimFinished = false;
+
+    bool Star2AnimFinished = false;
+
+    bool Star3AnimFinished = false;
+
+    float animTimeElapsed = 0;
+
+    float AnimCooldownTimer = 0.5f;
 
     private void Start()
     {
@@ -67,6 +89,18 @@ public class GameManager : MonoBehaviour
         audioSettingsManager.Load();
 
         AudioSettingsPanel.SetActive(false);
+
+        VictoryBGM.SetActive(false);
+
+        MakeScreenDarkerPanel.SetActive(false);
+
+        EndGameUI.SetActive(false);
+
+        Star1.GetComponent<RectTransform>().sizeDelta = new Vector2(0,0);
+
+        Star2.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
+
+        Star3.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
     }
 
     // Update is called once per frame
@@ -78,7 +112,7 @@ public class GameManager : MonoBehaviour
         {
             if (!GameOver)
             {
-                BGM.SetActive(false);
+                InGameBGM.SetActive(false);
 
                 CropSoundManager.instance.PlaySoundFXClip(GameOverSound, transform,audioSettingsManager.GetSFX());
 
@@ -132,11 +166,15 @@ public class GameManager : MonoBehaviour
         {
             CurrentWave += 1;
             currentWave.text = CurrentWave.ToString();
-            LocustCount = 10;
+            LocustCount = LocustMaxCount;
             LocustSpawnCount = 0;
         }
         else if(LocustCount == 0 && CurrentWave == 2)
         {
+            InGameBGM.SetActive(false);
+
+            VictoryBGM.SetActive(true);
+
             if (playerController != null)
             {
                 playerController.enabled = false;
@@ -160,6 +198,60 @@ public class GameManager : MonoBehaviour
             MakeScreenDarkerPanel.SetActive(true);
             
             EndGameUI.SetActive(true);
+
+            if (AnimCooldownTimer > 0)
+            AnimCooldownTimer -= Time.deltaTime;
+
+            if (!Star1AnimFinished)
+            {
+                Star1.SetActive(true);
+
+                animTimeElapsed += Time.deltaTime;
+
+                if(animTimeElapsed < 1)
+                {
+                    Star1.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(new Vector2(0,0),new Vector2(100,100),animTimeElapsed/1);
+                }
+                else
+                {
+                    Star1AnimFinished = true;
+                    animTimeElapsed = 0;
+                    AnimCooldownTimer = 1;
+                }
+            }
+            else if(Star1AnimFinished && !Star2AnimFinished && AnimCooldownTimer <= 0)
+            {
+                Star2.SetActive(true);
+
+                animTimeElapsed += Time.deltaTime;
+
+                if (animTimeElapsed < 1)
+                {
+                    Star2.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(new Vector2(0, 0), new Vector2(100, 100), animTimeElapsed / 1);
+                }
+                else
+                {
+                    Star2AnimFinished = true;
+                    animTimeElapsed = 0;
+                    AnimCooldownTimer = 1;
+                }
+            }
+            else if (Star2AnimFinished && !Star3AnimFinished && AnimCooldownTimer <= 0)
+            {
+                Star3.SetActive(true);
+
+                animTimeElapsed += Time.deltaTime;
+
+                if (animTimeElapsed < 1)
+                {
+                    Star3.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(new Vector2(0, 0), new Vector2(100, 100), animTimeElapsed / 1);
+                }
+                else
+                {
+                    Star3AnimFinished = true;
+                    animTimeElapsed = 0;
+                }
+            }
         }
 
         PlayerHealthBar.rectTransform.sizeDelta = new Vector2(1.8f * playerData.health, 38);
