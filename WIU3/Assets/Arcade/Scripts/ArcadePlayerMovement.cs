ce1a6746f07d72de8abca40c06b9294e42a2677e
SirPoopsalot1 <ngimmanuel979@gmail.com>
2024-08-20 11:23:59 +0800

    Update

--- WIU3/Assets/Arcade/Scripts/ArcadePlayerMovement.cs
+++ WIU3/Assets/Arcade/Scripts/ArcadePlayerMovement.cs
@@ -7,10 +7,12 @@ public class ArcadePlayerMovement : MonoBehaviour
     public float movspeed;
     float speedx, speedy;
     Rigidbody2D rb;
+    [SerializeField] private PauseMenuUI Texting;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Texting.TextPlay("This is the arcade in this place you can start playing our arcade games whilst playing the arcade games you will learn some topics regarding sustainability ARE YOU READY TO GET YOUR GAME ON!!!");
     }
 
     // Update is called once per frame
--- WIU3/Assets/Shared/Scripts/UI/PauseMenuUI.cs
+++ WIU3/Assets/Shared/Scripts/UI/PauseMenuUI.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,6 +14,8 @@ public class PauseMenuUI : MonoBehaviour
     public GameObject ControlmenuUI;
     public GameObject CreditmenuUI;
     public GameObject pauseMenuUI;
+    public GameObject DialogueUI;
+    public TextMeshProUGUI DialogueTexter;
     
     // Update is called once per frame
     void Update()
@@ -74,5 +78,53 @@ public class PauseMenuUI : MonoBehaviour
     {
         Pause();
         CurrentGameMenuUI.SetActive(false);
+    }
+    public void TextPlay(string DialogueText)
+    {
+        DialogueUI.SetActive(true);
+        StartCoroutine(TypeText(DialogueText));
+        //Add voices
+    }
+    private IEnumerator TypeText(string fullText)
+    {
+        string currentLine = ""; // Current line being formed
+        int charCount = 0; // Count of characters in the current line
+
+        // Loop through each character in the full text
+        foreach (char letter in fullText)
+        {
+            // Check if adding the next character would exceed the character limit
+            if (charCount >= 86 && letter != ' ') // Avoid breaking words
+            {
+                DialogueTexter.text = currentLine + "\n" + "~~Press SPACE To Continue~~";
+                // Wait for user input to continue to the next line
+                yield return WaitForInput();
+
+                // Clear the current line for the next sentence
+                currentLine = "";
+                charCount = 0; // Reset character count after clearing
+            }
+
+            // Add the letter to the current line
+            currentLine += letter;
+            charCount++; // Update character count
+
+            // Update the TextMeshPro text to show typed characters
+            DialogueTexter.text = currentLine; // Append prompt message;
+            yield return new WaitForSeconds(0.1f); // Wait for a specified duration
+        }
+        DialogueTexter.text = currentLine + "\n" + "~~Press SPACE To Continue~~";
+        // Final wait for input before finishing
+        yield return WaitForInput();
+        DialogueTexter.text = ""; // Clear text after final input
+        DialogueUI.SetActive(false);
+    }
+    private IEnumerator WaitForInput()
+    {
+        // Wait for user input to clear text and continue
+        while (!Input.GetKeyDown(KeyCode.Space)) // Change KeyCode.Space to any key you prefer
+        {
+            yield return null; // Wait until the input is detected
+        }
     }
 }
